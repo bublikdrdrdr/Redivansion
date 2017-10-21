45a2fee74cdee1a4392aa287addcb0794b0df843
Vadym Borys <bublik.drdrdr@gmail.com>
2017-10-21 02:09:53 +0200

    roadBuilder: added build function

--- app/src/main/java/tk/ubublik/redivansion/gamelogic/utils/game_tools/RoadBuilder.java
+++ app/src/main/java/tk/ubublik/redivansion/gamelogic/utils/game_tools/RoadBuilder.java
@@ -2,6 +2,7 @@ package tk.ubublik.redivansion.gamelogic.utils.game_tools;
 
 import android.graphics.Point;
 
+import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
 
 import java.util.Observable;
@@ -9,6 +10,7 @@ import java.util.Observer;
 
 import tk.ubublik.redivansion.gamelogic.camera.CameraControl;
 import tk.ubublik.redivansion.gamelogic.units.WorldMap;
+import tk.ubublik.redivansion.gamelogic.units.objects.Road;
 import tk.ubublik.redivansion.gamelogic.utils.MapRenderer;
 
 /**
@@ -98,12 +100,48 @@ public class RoadBuilder extends SelectTool implements Observer {
         this.selectStage = selectStage;
     }
 
-    public boolean build(){
-        return false;
+    public boolean build() {
+        return !(selectStage != SelectStage.END || !canPut()) && build(startPoint, endPoint);
     }
 
-    public boolean build(Point start, Point end){
-        return false;
+    private boolean build(Point start, Point end){
+        boolean result = true;
+        switch (getAxis(start, end)){
+            case NONE: throw new IllegalArgumentException("Points are not on one axis");
+            case X:
+                    if (start.y > end.y) {
+                        Point startCopy = start;
+                        start = new Point(end);
+                        end = new Point(startCopy);
+                    }
+                    for (int i = start.y; i <= end.y; i++) {
+                        if (!worldMap.put(new Road(new Point(start.x, i)))) result = false;
+                    }
+                    break;
+            case Y:
+                    if (start.x > end.x) {
+                        Point startCopy = start;
+                        start = new Point(end);
+                        end = new Point(startCopy);
+                    }
+                    for (int i = start.x; i <= end.x; i++)
+                        if (!worldMap.put(new Road(new Point(i, start.y)))) result = false;
+                    break;
+        }
+        resetRoadStates(start, end);
+        return result;
+    }
+
+    private void resetRoadStates(Point startPoint, Point endPoint){
+        Road.updateRoadStates(startPoint, endPoint, worldMap.getNearbyRoads(startPoint, endPoint));
+    }
+
+    private enum Axis{NONE, X, Y}
+
+    private Axis getAxis(Point p1, Point p2){
+        if (p1.x == p2.x) return Axis.X;
+        if (p1.y == p2.y) return Axis.Y;
+        return Axis.NONE;
     }
 
 
