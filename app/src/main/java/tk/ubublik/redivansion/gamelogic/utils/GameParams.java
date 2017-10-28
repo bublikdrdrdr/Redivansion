a77dd3db516a0d52532160968a48d78088bce463
Vadym Borys <bublik.drdrdr@gmail.com>
2017-10-28 11:46:04 +0200

    added game params

---
+++ app/src/main/java/tk/ubublik/redivansion/gamelogic/utils/GameParams.java
@@ -0,0 +1,15 @@
+package tk.ubublik.redivansion.gamelogic.utils;
+
+/**
+ * Created by Bublik on 28-Oct-17.
+ */
+
+public class GameParams {
+
+    public static final int OFFICE_BUILD_COST = 1000;
+    public static final int HOUSE_BUILD_COST = 1000;
+    public static final int ROAD_BUILD_COST = 1000;
+    public static final int TREE_BUILD_COST = 1000;
+    public static final int THERMAL_POWER_PLANT_BUILD_COST = 1000;
+
+}
