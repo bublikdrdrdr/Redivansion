e4d20c6970c4c915871f6a9455398337051e52bd
Vadym Borys <bublik.drdrdr@gmail.com>
2017-10-28 12:20:25 +0200

    added object level factory methods

--- app/src/main/java/tk/ubublik/redivansion/gamelogic/utils/GameParams.java
+++ app/src/main/java/tk/ubublik/redivansion/gamelogic/utils/GameParams.java
@@ -12,4 +12,13 @@ public class GameParams {
     public static final int TREE_BUILD_COST = 1000;
     public static final int THERMAL_POWER_PLANT_BUILD_COST = 1000;
 
+    public static final int OFFICE_DEFAULT_MONTH_COST = 100;
+    public static final int OFFICE_DEFAULT_POWER = 100;
+    public static final int OFFICE_DEFAULT_FIRE = 100;
+    public static final int OFFICE_DEFAULT_WATER = 100;
+    public static final int OFFICE_DEFAULT_POLLUTION = 100;
+    public static final int OFFICE_DEFAULT_MONEY = 100;
+    public static final int OFFICE_DEFAULT_CRIMINAL = 100;
+    public static final int OFFICE_DEFAULT_HEALTH = 100;
+
 }
