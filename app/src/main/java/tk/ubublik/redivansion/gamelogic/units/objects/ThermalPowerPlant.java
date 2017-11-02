9ea16ffee854e4ce9e2a0f1e2d1a880050922aaf
Vadym Borys <bublik.drdrdr@gmail.com>
2017-11-03 00:12:44 +0100

    added ThermalPowerPlant logic

--- app/src/main/java/tk/ubublik/redivansion/gamelogic/units/objects/ThermalPowerPlant.java
+++ app/src/main/java/tk/ubublik/redivansion/gamelogic/units/objects/ThermalPowerPlant.java
@@ -2,6 +2,7 @@ package tk.ubublik.redivansion.gamelogic.units.objects;
 
 import android.graphics.Point;
 
+import tk.ubublik.redivansion.gamelogic.graphics.GeometryAnimationManager;
 import tk.ubublik.redivansion.gamelogic.graphics.GeometryManager;
 import tk.ubublik.redivansion.gamelogic.utils.GameParams;
 
@@ -11,36 +12,6 @@ import tk.ubublik.redivansion.gamelogic.utils.GameParams;
 
 public class ThermalPowerPlant extends PowerPlant {
 
-    @Override
-    public void recalculateParams() {
-
-    }
-
-    @Override
-    public int getLevelsCount() {
-        return 0;
-    }
-
-    @Override
-    public void setLevelNumber(int level) {
-
-    }
-
-    @Override
-    public int getLevelNumber() {
-        return 0;
-    }
-
-    @Override
-    public int getMoneyDelta() {
-        return 0;
-    }
-
-    @Override
-    public int getUpgradeCost() {
-        return 0;
-    }
-
     public ThermalPowerPlant(){
         this(new Point());
     }
@@ -49,7 +20,7 @@ public class ThermalPowerPlant extends PowerPlant {
         setPosition(position);
         setSize(3);
         setNeedsRoad(true);
-        setBuildCost(GameParams.THERMAL_POWER_PLANT_BUILD_COST);
+        setBuildCost(GameParams.THERMAL_POWER_PLANT_LEVELS_BUILD_COST[0]);
     }
 
     @Override
@@ -60,5 +31,70 @@ public class ThermalPowerPlant extends PowerPlant {
     @Override
     public void parseBytes() {
 
+    }
+
+    private void beginAnimation(String animationName){
+        ((GeometryAnimationManager)getGeometryManager()).beginAnimation(animationName);
+    }
+
+    private void beginAnimation(String animationName, final String nextAnimation){
+        ((GeometryAnimationManager)getGeometryManager()).beginAnimation(animationName, new GeometryManager.OnAnimationEndListener() {
+            @Override
+            public void animationEnd() {
+                ((GeometryAnimationManager)getGeometryManager()).beginAnimation(nextAnimation);
+            }
+        });
+    }
+
+    /*
+    logic block
+     */
+
+    @Override
+    public void recalculateParams() {
+        setLevelParams();
+    }
+
+    @Override
+    public int getLevelsCount() {
+        return GameParams.THERMAL_POWER_PLANT_LEVELS_BUILD_COST.length;
+    }
+
+    @Override
+    public void setLevelNumber(int level) {
+        if (level<0 || level>=getLevelsCount()) throw new IllegalArgumentException("Wrong level number: "+level);
+        beginAnimation("fastDestroy"+Integer.toString(getLevelNumber()), "fastBuild"+Integer.toString(level));
+        this.level = level;
+        setLevelParams();
+    }
+
+    private void setLevelParams(){
+        setParams(GameParams.THERMAL_POWER_PLANT_LEVELS_MONTH_COST[level],
+                GameParams.THERMAL_POWER_PLANT_LEVELS_POWER[level],
+                GameParams.THERMAL_POWER_PLANT_LEVELS_FIRE[level],
+                GameParams.THERMAL_POWER_PLANT_LEVELS_WATER[level],
+                GameParams.THERMAL_POWER_PLANT_LEVELS_POLLUTION[level],
+                GameParams.THERMAL_POWER_PLANT_LEVELS_CRIMINAL[level],
+                GameParams.THERMAL_POWER_PLANT_LEVELS_HEALTH[level],
+                GameParams.THERMAL_POWER_PLANT_LEVELS_WORK[level],
+                GameParams.THERMAL_POWER_PLANT_LEVELS_HAPPINESS[level],
+                GameParams.THERMAL_POWER_PLANT_LEVELS_EDUCATION[level],
+                GameParams.THERMAL_POWER_PLANT_LEVELS_RADIUS[level]);
+    }
+
+    @Override
+    public int getLevelNumber() {
+        return level;
+    }
+
+    @Override
+    public int getMoneyDelta() {
+        return monthCost;
+    }
+
+    @Override
+    public int getUpgradeCost() {
+        if (level==GameParams.THERMAL_POWER_PLANT_LEVELS_BUILD_COST.length-1) return 0;
+        return GameParams.THERMAL_POWER_PLANT_LEVELS_BUILD_COST[level+1];
     }
 }
