35ae71abb1207317c98aed6141c2723d4f753e3c
Vadym Borys <bublik.drdrdr@gmail.com>
2017-11-06 10:49:59 +0100

    added some objects (empty classes)

---
+++ app/src/main/java/tk/ubublik/redivansion/gamelogic/units/objects/FireStation.java
@@ -0,0 +1,47 @@
+package tk.ubublik.redivansion.gamelogic.units.objects;
+
+/**
+ * Created by Bublik on 06-Nov-17.
+ */
+
+public class FireStation extends WorldObject {
+    @Override
+    public void recalculateParams() {
+
+    }
+
+    @Override
+    public int getLevelsCount() {
+        return 0;
+    }
+
+    @Override
+    public void setLevelNumber(int level) {
+
+    }
+
+    @Override
+    public int getLevelNumber() {
+        return 0;
+    }
+
+    @Override
+    public int getMoneyDelta() {
+        return 0;
+    }
+
+    @Override
+    public int getUpgradeCost() {
+        return 0;
+    }
+
+    @Override
+    public byte[] toBytes() {
+        return new byte[0];
+    }
+
+    @Override
+    public void parseBytes() {
+
+    }
+}
