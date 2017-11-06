9691d660750bf7b8777d5a06b9017fd469c62ab6
Vadym Borys <bublik.drdrdr@gmail.com>
2017-11-06 10:48:35 +0100

    changed button texture

--- app/src/main/java/tk/ubublik/redivansion/gamelogic/gui/Frames.java
+++ app/src/main/java/tk/ubublik/redivansion/gamelogic/gui/Frames.java
@@ -21,14 +21,14 @@ public class Frames {
 
     private void initMain(){
         main = new GUIFrames("main");
-        main.addElement("addSmthing", "+", 0, (80 * dY), (20 * dY), (20 * dY), "pics/test.jpg");
+        main.addElement("addSmthing", "+", 0, (80 * dY), (20 * dY), (20 * dY), "Textures/Monkey.png");
         main.addElement("showSelect", "Select", 0, (60 * dY), (20 * dY), (20 * dY), "pics/bg.jpg");
         main.addElement("remove", "Remove", 0, (40 * dY), (20 * dY), (20 * dY), "pics/bg.jpg");
     }
 
     private void initSelect(){
         select = new GUIFrames("select");
-        select.addElement("bg", "", (30 * dX), (10 * dY), (40 * dX), (80 * dY), "pics/test.jpg");
+        select.addElement("bg", "", (30 * dX), (10 * dY), (40 * dX), (80 * dY), "Textures/Monkey.png");
         select.addElement("selectTree", "Select Tree", (32 * dX), (72 * dY), (36 * dX), (16 * dY), "pics/bg.jpg");
         select.addElement("selectOffice", "Select Office", (32 * dX), (52 * dY), (36 * dX), (16 * dY), "pics/bg.jpg");
         select.addElement("selectClear", "Clear Select", (32 * dX), (32 * dY), (36 * dX), (16 * dY), "pics/bg.jpg");
@@ -37,7 +37,7 @@ public class Frames {
 
     private void initAdd(){
         add = new GUIFrames("add");
-        add.addElement("bg", "", (30 * dX), (5 * dY), (40 * dX), (90 * dY), "pics/test.jpg");
+        add.addElement("bg", "", (30 * dX), (5 * dY), (40 * dX), (90 * dY), "Textures/Monkey.png");
         add.addElement("addTree", "Add Tree", 32 * dX, 78 * dY, 36 * dX, 16 * dY, "pics/bg.jpg");
         add.addElement("addOffice", "Add Office", 32 * dX, 60 * dY, 36 * dX, 16 * dY, "pics/bg.jpg");
         add.addElement("addRoad", "Add Road", 32 * dX, 42 * dY, 36 * dX, 16 * dY, "pics/bg.jpg");
