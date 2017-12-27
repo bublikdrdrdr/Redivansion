0c3754b501bde0110bf50275dd0e8118a33e07c5
qwesdcx <31733044+qwesdcx@users.noreply.github.com>
2017-12-27 15:54:11 +0200

    fixed back button and level list

--- app/src/main/java/tk/ubublik/redivansion/gamelogic/lifecycle/MenuLifecycle.java
+++ app/src/main/java/tk/ubublik/redivansion/gamelogic/lifecycle/MenuLifecycle.java
@@ -60,62 +60,22 @@ public class MenuLifecycle extends Lifecycle implements TouchListener {
 
     @Override
     public void update() {
-        //loadingPanel.setLocalTranslation(FastMath.nextRandomInt(0, simpleApplication.getCamera().getWidth()), FastMath.nextRandomInt(0, simpleApplication.getCamera().getHeight()), 0);
+
     }
 
     Spatial background;
     private void createMenuElements(){
-        //addBackgroundImage();
 
-        final Container myWindow = new Container();
-        simpleApplication.getGuiNode().attachChild(myWindow);
         settings = Settings.getInstance();
         settings.open();
         gui = new GUI(simpleApplication.getGuiNode(), null, null, AllFrames.mainMenu);
+        Main.registerBackPressListener(gui.touchListener, simpleApplication.getInputManager());
         done = false;
-
-        /*myWindow.setLocalScale(4f);
-        myWindow.setLocalTranslation(simpleApplication.getCamera().getWidth()/2-200, simpleApplication.getCamera().getHeight()-200, 0);
-        Label label = myWindow.addChild(new Label("Main menu"));
-        label.setColor(ColorRGBA.Cyan);
-        Button startGame = myWindow.addChild(new Button("Start game"));
-        Button tutorial = myWindow.addChild(new Button("Tutorial"));
-        Button settings = myWindow.addChild(new Button("Settings"));
-        Button exit = myWindow.addChild(new Button("Exit"));
-        startGame.addClickCommands(new Command<Button>() {
-            @Override
-            public void execute(Button source) {
-
-            }
-        });
-        tutorial.addClickCommands(new Command<Button>() {
-            @Override
-            public void execute(Button source) {
-                menuResult = MenuResult.START_TUTORIAL;
-                done = true;
-            }
-        });
-        exit.addClickCommands(new Command<Button>() {
-            @Override
-            public void execute(Button source) {
-                menuResult = MenuResult.EXIT;
-                done = true;
-            }
-        });*/
     }
 
     public static void buttonClicked(MenuResult result){
         menuResult = result;
         done = true;
     }
-
-    /*private void addBackgroundImage(){
-        Picture backgroundPicture = (Picture)NodesCache.getInstance().get("menu_background");
-        backgroundPicture.setWidth(simpleApplication.getCamera().getWidth());
-        backgroundPicture.setHeight(simpleApplication.getCamera().getHeight());
-        backgroundPicture.setPosition(0,0);
-        simpleApplication.getGuiNode().attachChild(backgroundPicture);
-    }*/
-
 }
 
